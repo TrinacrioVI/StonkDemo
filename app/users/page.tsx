a9294a1bd60701e4a29b9215f8bf71679e1113cb
trinacrioVI <trinacriocdxx@gmail.com>
2024-06-14 15:14:23 -0700

    "layout experiment"

---
+++ app/users/page.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+export default function UsersPage() {
+  return(
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3xl font-bold ">
+          All Users
+        </h1>
+      </div>
+    </section>
+  )
+}
